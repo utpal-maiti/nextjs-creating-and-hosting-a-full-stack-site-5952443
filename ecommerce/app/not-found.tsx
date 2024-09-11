31934454abdba986b7a6f0c54a484feaf3a35342
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-11 23:43:15 +0000

    Added styles to the rest of the app

--- ecommerce/app/not-found.tsx
+++ ecommerce/app/not-found.tsx
@@ -1,7 +1,8 @@
 export default function NotFoundPage() {
   return (
-    <>
-    <h1>Not Found</h1>
-    </>
+    <div className="container mx-auto p-8 flex flex-col items-center justify-center h-screen"> 
+      <h1 className="text-4xl font-bold mb-4">Not Found</h1> 
+      <p className="text-gray-600">The page you are looking for does not exist.</p> 
+    </div>
   );
 }
