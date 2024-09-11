e778454f4e656734359156a3c40c4f8031685b43
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-11 23:59:58 +0000

    Added a navbar

--- ecommerce/app/layout.tsx
+++ ecommerce/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NavBar from "./NavBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        
+        <NavBar />
         {children}
       </body>
     </html>
