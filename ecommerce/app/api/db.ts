a21651b450c9c3b8ef8d80bd3d7a5b4269d61dac
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-12 19:30:24 +0000

    Added a db file for connecting to MongoDB

---
+++ ecommerce/app/api/db.ts
@@ -0,0 +1,19 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+export async function connectToDb() {
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.vqdwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  try {
+    await client.connect();
+  } finally {
+    await client.close();
+  }
+}
