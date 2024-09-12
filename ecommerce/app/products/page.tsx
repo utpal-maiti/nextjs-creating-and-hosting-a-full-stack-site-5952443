2e00137c814b2444d3d26a42da9328cf49859f1f
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-12 22:23:53 +0000

    Added fetching to the products page

--- ecommerce/app/products/page.tsx
+++ ecommerce/app/products/page.tsx
@@ -1,7 +1,9 @@
 import ProductsList from "../ProductsList";
-import { products } from '../product-data';
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  const response = await fetch('https://didactic-spork-qv5rg559vw73xgq9-3000.app.github.dev/api/products');
+  const products = await response.json();
+
   return (
     <div className="container mx-auto p-8"> 
       <h1 className="text-4xl font-bold mb-8">Products</h1> 
