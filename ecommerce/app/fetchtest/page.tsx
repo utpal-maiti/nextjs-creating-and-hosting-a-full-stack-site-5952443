73b5915f981f62659704193062a2c811d6a3ec2c
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-13 21:32:05 +0000

    Removed fetchtest

--- ecommerce/app/fetchtest/page.tsx
+++
@@ -1,6 +0,0 @@
-export default async function FetchTest() {
-  const response = await fetch(process.env.NEXT_PUBLIC_SITE_URL + '/api/hello');
-  const data = await response.json();
-
-  return <h1>{data.message}</h1>
-}
